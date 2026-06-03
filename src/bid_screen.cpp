e4d1cf8cdf7d2095660aa9bddda12cbea2c5ce7c
Primul Gairapiple <primul1625@student.ku.edu.np>
2026-06-03 23:22:40 +0545

    Completed bid selection screen UI

--- src/bid_screen.cpp
+++ src/bid_screen.cpp
@@ -1,75 +1,72 @@
 #include <raylib.h>
 
-int main()
+int selectedBid = 0;
+
+void DrawBidScreen(Texture2D bg)
 {
-    InitWindow(1200, 800, "Call Break Bid");
+    Vector2 mouse = GetMousePosition();
 
-    Texture2D bg = LoadTexture("Assets/background.png");
-
-    int selectedBid = 0;
-
-    while (!WindowShouldClose())
+    Rectangle buttons[8] =
     {
-        Vector2 mouse = GetMousePosition();
+        {150,250,100,100},
+        {330,250,100,100},
+        {510,250,100,100},
+        {690,250,100,100},
 
-        Rectangle buttons[8] =
+        {150,450,100,100},
+        {330,450,100,100},
+        {510,450,100,100},
+        {690,450,100,100}
+    };
+
+    for(int i = 0; i < 8; i++)
+    {
+        if(CheckCollisionPointRec(mouse, buttons[i]) &&
+           IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
-            {180,250,100,100},
-            {380,250,100,100},
-            {580,250,100,100},
-            {780,250,100,100},
-
-            {180,450,100,100},
-            {380,450,100,100},
-            {580,450,100,100},
-            {780,450,100,100}
-        };
-
-        for(int i=0;i<8;i++)
-        {
-            if(CheckCollisionPointRec(mouse,buttons[i]) &&
-               IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            {
-                selectedBid = i+1;
-            }
+            selectedBid = i + 1;
         }
-
-        BeginDrawing();
-
-        DrawTexture(bg,0,0,WHITE);
-
-        DrawText("ENTER YOUR BID",400,80,50,GOLD);
-
-        for(int i=0;i<8;i++)
-        {
-            Color c = LIGHTGRAY;
-
-            if(selectedBid==i+1)
-                c = YELLOW;
-
-            DrawCircle(buttons[i].x+50,
-                       buttons[i].y+50,
-                       50,
-                       c);
-
-            DrawText(TextFormat("%d",i+1),
-                     buttons[i].x+35,
-                     buttons[i].y+25,
-                     40,
-                     BLACK);
-        }
-
-        DrawText(
-            TextFormat("Selected Bid: %d",selectedBid),
-            430,
-            700,
-            35,
-            GREEN
-        );
-
-        EndDrawing();
     }
 
-    UnloadTexture(bg);
-    CloseWindow();
+    DrawTexturePro(
+        bg,
+        (Rectangle){0,0,(float)bg.width,(float)bg.height},
+        (Rectangle){0,0,900,900},
+        (Vector2){0,0},
+        0,
+        WHITE
+    );
+
+    DrawText("ENTER YOUR BID", 250, 100, 45, GOLD);
+
+    for(int i = 0; i < 8; i++)
+    {
+        Color btnColor = LIGHTGRAY;
+
+        if(selectedBid == i + 1)
+            btnColor = YELLOW;
+
+        DrawCircle(
+            buttons[i].x + 50,
+            buttons[i].y + 50,
+            50,
+            btnColor
+        );
+
+        DrawText(
+            TextFormat("%d", i + 1),
+            buttons[i].x + 35,
+            buttons[i].y + 25,
+            40,
+            BLACK
+        );
+    }
+
+    DrawText(
+        TextFormat("Selected Bid : %d", selectedBid),
+        300,
+        700,
+        35,
+        GREEN
+    );
 }
