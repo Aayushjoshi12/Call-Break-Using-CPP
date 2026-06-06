b47e18be72112994bdf5802e1d3409a7fe19897c
Aayushjoshi12 <100678671+Aayushjoshi12@users.noreply.github.com>
2026-06-06 17:17:40 +0545

    style:Change format

--- total.cpp
+++ total.cpp
@@ -4,7 +4,7 @@ int main()
 {
     InitWindow(1200, 800, "Final Result");
     SetTargetFPS(60);
-     Font myFont = LoadFont("Cinzel-Bold.ttf");
+    Font myFont = LoadFont("Cinzel-Bold.ttf");
     int scores[5][4] = {0};
 
     while (!WindowShouldClose())
@@ -25,26 +25,25 @@ int main()
         DrawTextEx(myFont, "FINAL RESULT", {520, 40}, 40, 2, GOLD);
 
         for (int p = 0; p < 4; p++)
-        DrawTextEx(myFont,TextFormat("PLAYER %d", p + 1),{ (float)(x[p] - 40), 120 },25, 2, WHITE);
-           
+            DrawTextEx(myFont, TextFormat("PLAYER %d", p + 1), {(float)(x[p] - 40), 120}, 25, 2, WHITE);
 
         for (int r = 0, y = 220; r < 5; r++, y += 90)
         {
-            DrawTextEx(myFont, TextFormat("ROUND %d", r + 1),{100,(float) y}, 28, 2, WHITE);
+            DrawTextEx(myFont, TextFormat("ROUND %d", r + 1), {100, (float)y}, 28, 2, WHITE);
 
             for (int p = 0; p < 4; p++)
                 if (scores[r][p])
-                   DrawTextEx(myFont, TextFormat("%d", scores[r][p]),{(float)x[p], (float)y}, 28, 2, YELLOW);
+                    DrawTextEx(myFont, TextFormat("%d", scores[r][p]), {(float)x[p], (float)y}, 28, 2, YELLOW);
         }
 
-          DrawTextEx(myFont, "TOTAL",{100, 680}, 32, 2, WHITE);
+        DrawTextEx(myFont, "TOTAL", {100, 680}, 32, 2, WHITE);
 
         for (int p = 0; p < 4; p++)
             if (total[p])
-                DrawTextEx(myFont, TextFormat("%d", total[p]),{(float)x[p], 680}, 32, 2, GREEN);
+                DrawTextEx(myFont, TextFormat("%d", total[p]), {(float)x[p], 680}, 32, 2, GREEN);
 
         if (total[winner])
-            DrawTextEx(myFont,TextFormat("CONGRATULATIONS PLAYER %d ON WINNING", winner + 1),{250, 720}, 28, 2, WHITE);
+            DrawTextEx(myFont, TextFormat("CONGRATULATIONS PLAYER %d ON WINNING", winner + 1), {250, 720}, 28, 2, WHITE);
 
         EndDrawing();
     }
