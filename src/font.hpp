503406cb85d3447a6b9b70f873d2c8c7df72ad22
rajeshkhadka007 <rajeshkhadka1199@gmail.com>
2026-06-13 15:40:30 +0545

    call break
    mmm

--- src/font.hpp
+++ src/font.hpp
@@ -4,6 +4,7 @@ class fonta{
     fonta(const char *fontpath, int fontSize);
     ~fonta();
     void drawf(const char *text, Vector2 position, float fontSize, float spacing, Color tint);
+
     private:
     Font font;
         Vector2 posa;
