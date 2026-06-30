bc92268e1f9e9c103af6a35461db3de006c441db
Primul Gairapiple <primul1625@student.ku.edu.np>
2026-06-30 23:26:07 +0545

    Update bid_screen.h

--- src/bid_screen.h
+++ src/bid_screen.h
@@ -6,16 +6,64 @@
 class BidScreen
 {
 private:
-    int selectedBid;
-    Rectangle bidButtons[8];
+
+    Texture2D backgroundTexture;
+
+    static const int TOTAL_BID_BUTTONS = 8;
+
+    Rectangle bidButtonRectangle[TOTAL_BID_BUTTONS];
+
+    Rectangle confirmButtonRectangle;
+
+    int selectedBidNumber;
+
+    bool bidConfirmed;
+
+    bool showWarningMessage;
+
+    const char* warningMessage;
 
 public:
+
     BidScreen();
 
-    void Update();
-    void Draw(Texture2D backgroundImage);
+    ~BidScreen();
 
-    int GetSelectedBid();
+    void Update();
+
+    void Draw();
+
+    bool IsBidConfirmed() const;
+
+    int GetSelectedBid() const;
+
+private:
+
+    void LoadResources();
+
+    void UnloadResources();
+
+    void CreateLayout();
+
+    void HandleBidSelection();
+
+    void HandleConfirmButton();
+
+    void DrawBackground();
+
+    void DrawTitle();
+
+    void DrawInstructionText();
+
+    void DrawBidButtons();
+
+    void DrawSelectedBidPanel();
+
+    void DrawConfirmButton();
+
+    void DrawWarningMessage();
+
+    bool IsMouseOverButton(Rectangle buttonRectangle);
 };
 
 #endif
